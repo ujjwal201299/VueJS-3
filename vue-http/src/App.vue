6bbd3ff7deb99ecf1a89ddd9ee00fa3c7d9ad816
Ujjwal Jain <youjju2012@gmail.com>
2022-06-10 15:54:36 +0530

    http is Vue

---
+++ vue-http/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <CreatePost />
+  <PostList />
+  <hr />
+
+  <!-- Lifecycle Hooks -->
+  <Parent />
+  <hr />
+
+  <!-- Template Refs -->
+  <TemplateRef />
+  <hr />
+
+  <!-- mixins -->
+  <ClickCounter />
+  <HoverCounter />
+</template>
+
+<script>
+import PostList from "./components/PostList";
+import CreatePost from "./components/CreatePost";
+import Parent from "./components/Parent";
+import TemplateRef from "./components/TemplateRef";
+import ClickCounter from "./components/ClickCounter";
+import HoverCounter from "./components/HoverCounter";
+
+export default {
+  name: "App",
+  components: {
+    PostList,
+    CreatePost,
+    Parent,
+    TemplateRef,
+    ClickCounter,
+    HoverCounter,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-http/src/components/ClickCounter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button @click="incrementCount">Clicked {{ count }} times</button>
+  </div>
+</template>
+
+<script>
+import CounterMixin from "../mixins/counter";
+
+export default {
+  name: "ClickCounter",
+  mixins: [CounterMixin],
+};
+</script>
+
+<style scoped></style>
